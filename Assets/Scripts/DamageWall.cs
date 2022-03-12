353c527f33b0d2dc07c81d088dfe57ac76620345
Ayma-n <aymanbt14@gmail.com>
2022-03-12 15:43:55 -0500

    cleanup of DamageWall

--- Assets/Scripts/DamageWall.cs
+++ Assets/Scripts/DamageWall.cs
@@ -7,18 +7,6 @@ public class DamageWall : WallBehavior
 
     [SerializeField] private int wallDamage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     public override void StartWallCollisionBehavior(GameObject player) {
         MortalInfo playerInfo = player.GetComponent<MortalInfo>();
         playerInfo.TakeDamage(wallDamage);
