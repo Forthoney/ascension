73fb733de75bc44fafe43f63ac148cfe07aa6dba
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-03-12 13:11:31 -0500

    change public variables to Serialized private variables

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 { 
-    public float viewRange = 40f;
-    public float stopRange = 5f; 
-    public float turnRate = 5f;
-    public float moveRate = 1f;
-    public GameObject player;
+    [SerializeField] private float viewRange = 40f;
+    [SerializeField] private float stopRange = 5f; 
+    [SerializeField] private float turnRate = 5f;
+    [SerializeField] private float moveRate = 1f;
+    [SerializeField] private GameObject player;
 
-    float stepMove;
-    Transform target;
+    private float stepMove;
+    private Transform target;
 
     // Start is called before the first frame update
     void Start()
@@ -24,28 +25,31 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CanSeePlayer()) {
+        if (CanSeePlayer()) 
+        {
             FaceTarget();
-
-            if (Vector3.Distance(target.position, transform.position) > stopRange)
-            {
-                MoveToTarget();
-            }
+            MoveToTarget();
         }
     }
 
-    bool CanSeePlayer() {
+    private bool CanSeePlayer() 
+    {
         float distance = Vector3.Distance(target.position, transform.position);
         return (distance <= viewRange);
     }
 
-    void FaceTarget() {
+    private void FaceTarget() 
+    {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnRate);
     }
 
-    void MoveToTarget() {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, stepMove);
+    private void MoveToTarget() 
+    {
+        if (Vector3.Distance(target.position, transform.position) > stopRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, stepMove);
+        }
     }
 }
--- Assets/Scripts/HSWeaponBehavior.cs
+++ Assets/Scripts/HSWeaponBehavior.cs
@@ -6,33 +6,42 @@ using UnityEngine;
 // Watch video: https://www.youtube.com/watch?v=THnivyG0Mvo
 public class HSWeaponBehavior : WeaponBehavior
 {
-    public int damage = 10;
-    public float range = 100f;
-    public Camera fpsCam;
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float range = 100f;
+    [SerializeField] private Camera playerView;
+    [SerializeField] private WeaponMovement weaponMovement;
 
     public override void Shoot()
     {
-        // Bit shift the index of the layer (2, the ignore raycast layer) to get a bit mask
-        int layerMask = 1 << 2;
-
-        // We want to cast rays again every collider that is NOT in layer 2. So we inverse the bitmap.
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, layerMask);
-
-        if (hasHit) {
-            Debug.Log(hit.transform.tag);
-            if (hit.transform.tag == "Enemy") {
-                MortalInfo hitStats = hit.transform.GetComponent<MortalInfo>();
-                hitStats.TakeDamage(this.damage);
-            }
-        }
-
+        weaponMovement.ApplyRecoil();
+        ShootRay(playerView.transform);
         ResetCooldown();
     }
 
-    override public float GetKnockback()
+    private void ShootRay(Transform fpsCamera)
+    {
+        RaycastHit hit;
+        // Bit shift the index of the layer (2, the ignore raycast layer) to get a bit mask
+        int layerMask = 1 << 2;
+        // We want to cast rays again every collider that is NOT in layer 2. So we inverse the bitmap.
+        layerMask = ~layerMask;
+        if (Physics.Raycast(fpsCamera.position, fpsCamera.forward, out hit, range, layerMask)) //hit check
+        {
+            ApplyDamage(hit);
+        }
+    }
+
+    private void ApplyDamage(RaycastHit hit)
+    {
+        Debug.Log(hit.transform.tag);
+        if (hit.transform.CompareTag("Enemy")) 
+        {
+            MortalInfo hitStats = hit.transform.GetComponent<MortalInfo>();
+            hitStats.TakeDamage(this.damage);
+        }
+    }
+
+    public override float GetKnockback()
     {
         return 10f;
     }
