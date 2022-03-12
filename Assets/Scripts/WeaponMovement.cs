ad36e9138f91d23588ac331a870c8c5eea84a3b3
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-03-12 13:11:47 -0500

    implement recoil

--- Assets/Scripts/WeaponMovement.cs
+++ Assets/Scripts/WeaponMovement.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class WeaponMovement : MonoBehaviour
 {
     [Header("Movement Sway Settings")]
-    [SerializeField] private float smooth;
+    [SerializeField] private float smoothSway;
     [SerializeField] private float swayMultiplier;
-    float mouseX, mouseY;
-    Quaternion targetRotation;
-
-    // Update is called once per frame
+    [Header("Recoil Settings")]
+    [SerializeField] private float magnitude;
+    [SerializeField] private float recoilPeriod;
+    
     void Update()
     {
-        transform.localRotation = MovementSway(transform.localRotation);
+        transform.localRotation = SwayMovement(transform.localRotation);
     }
 
-    Quaternion MovementSway(Quaternion localRotation)
+    private Quaternion SwayMovement(Quaternion localRotation)
     {
-        mouseX = Input.GetAxisRaw("Mouse X") * swayMultiplier;
-        mouseY = Input.GetAxisRaw("Mouse Y") * swayMultiplier;
+        float mouseX = Input.GetAxisRaw("Mouse X") * swayMultiplier;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * swayMultiplier;
 
         Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
         Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
-        return Quaternion.Slerp(localRotation, rotationX * rotationY, smooth * Time.deltaTime);
+
+        return Quaternion.Slerp(localRotation, rotationX * rotationY, smoothSway * Time.deltaTime);
+    }
+    public void ApplyRecoil()
+    {
+        Quaternion recoilRotation = Quaternion.AngleAxis(magnitude, Vector3.up);
+        float elapsedTime = 0f;
+        while (elapsedTime < recoilPeriod) 
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, recoilRotation, recoilPeriod - elapsedTime);
+            elapsedTime += Time.deltaTime;
+        }
     }
 }
