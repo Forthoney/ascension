e85e11ae6e422b6f78643d392113516b948dfa9c
Ayma-n <aymanbt14@gmail.com>
2022-03-19 14:32:32 -0400

    Added functional weapon spread

--- Assets/Scripts/HSWeaponBehavior.cs
+++ Assets/Scripts/HSWeaponBehavior.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 // Watch video: https://www.youtube.com/watch?v=THnivyG0Mvo
 public class HSWeaponBehavior : WeaponBehavior
 {
-    
+
+    [Header("General Settings and Associations")]
     [SerializeField] private float range = 100f;
     [SerializeField] private Camera playerView;
     [SerializeField] private WeaponMovement weaponMovement;
 
+    [Header("Screen Shake Settings")]
     [SerializeField] private ScreenShake shaker;
     [SerializeField] private float shakeDuration = 0.1f;
     [SerializeField] private float shakeIntensity = 0.1f;
 
-    
+    [Header("Weapon Spread Settings")]
+    [SerializeField] private float maxAngleDeviation = 5f;
 
 
     public override void Shoot()
@@ -36,10 +39,37 @@ public class HSWeaponBehavior : WeaponBehavior
         int layerMask = 1 << 2;
         // We want to cast rays again every collider that is NOT in layer 2. So we inverse the bitmap.
         layerMask = ~layerMask;
-        if (Physics.Raycast(fpsCamera.position, fpsCamera.forward, out hit, range, layerMask)) //hit check
+
+        Vector3 deviatedRay = GetDeviatedRay(fpsCamera.forward);
+
+        if (Physics.Raycast(fpsCamera.position, deviatedRay, out hit, range, layerMask)) //hit check
         {
             ApplyDamage(hit);
+            if (hit.transform.tag == "Enemy") Debug.Log(Random.Range(1, 10000) + " shot an enemy");
         }
+
+        //Debug.Log(deviatedRay.ToString());
+        Debug.Log(fpsCamera.forward);
+        Debug.DrawRay(fpsCamera.position, 200 * deviatedRay, Color.red, 0.5f);
+    }
+
+    private Vector3 GetDeviatedRay(Vector3 forwardVector) {
+        Vector3 deviatedRay = forwardVector;
+
+        // Generates a value for the angle deviation from the normal forward vector (will determine r in polar coordinates).
+        float deviation = Random.Range(0f, maxAngleDeviation);
+
+        // Generates an angle between 0 and 360 degrees to determine where, on the deviation circle, the ray should be shot (theta in polar coordinates)
+        float angle = Random.Range(0f, 360f);
+
+        // Creates a Rotation around the y axis (Vector3.up) by "deviation" degrees and applies it to the forward vector.
+        deviatedRay = Quaternion.AngleAxis(deviation, Vector3.up) * deviatedRay;
+
+        // Creates a Rotation around the forward vector by "angle" degrees, and applies it to the deviated ray.
+        deviatedRay = Quaternion.AngleAxis(angle, forwardVector) * deviatedRay;
+
+        return deviatedRay;
+
     }
 
     private void ApplyDamage(RaycastHit hit)
