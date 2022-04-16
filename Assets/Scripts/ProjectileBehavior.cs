902a319157611aac0bcbc60f5767f9de05b3f735
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-04-16 15:17:11 -0400

    refactor ProjectileBehavior.cs

--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -6,39 +6,22 @@ public class ProjectileBehavior : MonoBehaviour
 {
     public int damage = 10;
     public float projectileSpeed = 10f;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        MoveProjectile();
+        transform.position += transform.forward * Time.deltaTime * projectileSpeed;
         CheckCollision();
     }
-
-    // Moves the projectile forward.
-    void MoveProjectile()
+    private void CheckCollision()
     {
-        transform.position += transform.forward * Time.deltaTime * projectileSpeed;
-    }
-
-    void CheckCollision()
-    {
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(transform.position, transform.forward, out hit, projectileSpeed * Time.deltaTime);
-        if (hasHit)
+        if (!Physics.Raycast(transform.position, transform.forward, out var hit,
+                projectileSpeed * Time.deltaTime)) return;
+        
+        if (hit.transform.gameObject.CompareTag("Player"))
         {
-            //Debug.Log(hit.transform.name);
-            // If the projectile hits a player, call its TakeDamage method.
-            if (hit.transform.name == "Player")
-            {
-                MortalInfo hitStats = hit.transform.GetComponent<MortalInfo>();
-                hitStats.TakeDamage(this.damage);
-            }
-            Destroy(this.gameObject);
+            hit.transform.GetComponent<MortalInfo>().TakeDamage(this.damage);
         }
+        Destroy(gameObject);
     }
 }
